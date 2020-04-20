9042a7e2932c7e4612d2301533c686fbaf602925
Ingon Lee <ingonlee7942@gmail.com>
2020-04-20 12:07:47 -0400

    Update DetailContainer

--- src/routes/Detail/DetailContainer.js
+++ src/routes/Detail/DetailContainer.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import DetailPresenter from './DetailPresenter';
+import { movieApi, tvApi } from 'api';
 
 class DetailContainer extends Component {
-  state = {
-    result: null,
-    loading: true,
-    error: null
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: null,
+      loading: true,
+      error: null,
+      isMovie: props.location.pathname.includes('/movie/')
+    }
+  }
+
+  componentDidMount() {
+    const id = parseInt(this.props.match.params.id);
+    const detailApi = this.state.isMovie ? movieApi.movieDetail : tvApi.showDetail;
+
+    if (isNaN(id)) {
+      this.props.history.push("/")
+    }
+
+    detailApi(id)
+      .then(response => {
+        this.setState({
+          result: response.data,
+          loading: false
+        })
+      })
+      .catch(error => {
+        this.setState({
+          error: "Can't find anything.",
+          loading: false
+        })
+      })
   }
 
   render() {
     const { result, loading, error } = this.state;
+    console.log(this.props)
 
     return (
       <DetailPresenter
