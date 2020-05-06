abe8c683b3688c04d619f3ea8cec1408993a1b8a
Ingon Lee <ingonlee7942@gmail.com>
2020-05-05 21:37:21 -0400

    Create Season component

---
+++ src/components/Season.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  padding-bottom: 15px;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.22);
+
+  :last-child {
+    margin-bottom: 0;
+  }
+
+  @media (max-width: 419px) {
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+`;
+
+const Image = styled.div`
+  flex: 0 0 125px;
+  height: 180px;
+  background: url(${props => props.bgImage}) center/cover no-repeat;
+  border-radius: 3px;
+`
+
+const DetailContainer = styled.div`
+  width: 100%;
+  margin-left: 15px;
+`
+
+const Info = styled.span`
+  display: block;
+  font-size: 18px;
+  margin-bottom: 10px;
+
+  @media (max-width: 419px) {
+    text-align: center;
+    margin-top: 10px;
+  }
+`
+
+const EpisodeCount = styled.span`
+  display: block;
+  margin-bottom: 10px;
+
+  @media (max-width: 419px) {
+    text-align: center;
+  }
+`
+
+const SeasonOverview = styled.span`
+  display: block;
+  opacity: 0.7;
+  line-height: 1.5;
+`
+
+function Season({ imgUrl, name, year, epCount, overview }) {
+  const bgUrl = imgUrl ? `https://image.tmdb.org/t/p/w300${imgUrl}` : require('../assets/not-available.png');
+  return (
+    <Container>
+      <Image bgImage={bgUrl} />
+      <DetailContainer>
+        <Info>
+          {name} {year && `(${year})`}
+        </Info>
+        <EpisodeCount>
+          {epCount} Episodes
+        </EpisodeCount>
+        <SeasonOverview>
+          {overview}
+        </SeasonOverview>
+      </DetailContainer>
+    </Container>
+  )
+};
+
+Season.propTypes = {
+  imgUrl: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  year: PropTypes.string,
+  epCount: PropTypes.number,
+  overview: PropTypes.string
+}
+
+export default Season;
