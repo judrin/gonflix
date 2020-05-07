b6bec919157cb033db973433007dfb050d1daa70
Ingon Lee <ingonlee7942@gmail.com>
2020-05-07 17:00:27 -0400

    Updates

---
+++ precache-manifest.cc210b9e07c9b70cb0dd2beee5402e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3707f8b96889a05ef261238dcaf2cf",
+    "url": "/gonflix/index.html"
+  },
+  {
+    "revision": "1c311cbd76b21b56a6ef",
+    "url": "/gonflix/static/js/2.a000f4ca.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/gonflix/static/js/2.a000f4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cee35f25f2ef3c835d6",
+    "url": "/gonflix/static/js/main.f1516ac2.chunk.js"
+  },
+  {
+    "revision": "17d4fa6f87462bde7f1f",
+    "url": "/gonflix/static/js/runtime-main.e0ff08be.js"
+  },
+  {
+    "revision": "00dfd1d07c275b592b48438298bc4d35",
+    "url": "/gonflix/static/media/not-available.00dfd1d0.png"
+  }
+]);
